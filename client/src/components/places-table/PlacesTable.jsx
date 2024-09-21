4f60293cab9825796c63a209c7356ddf34233e1b
webosmotic-developer <dev.webosmotic@gmail.com>
2024-09-21 14:45:47 +0530

    fix: optimizations based on slandered

--- client/src/components/places-table/PlacesTable.jsx
+++ client/src/components/places-table/PlacesTable.jsx
@@ -8,19 +8,34 @@ const columns = [
   { label: "Country", key: "country" },
 ];
 
-const PlacesTable = ({ places, errorMessage, isLoading }) => {
+const PlacesTable = ({ places = [], errorMessage, isLoading }) => {
   if (isLoading) return <Spinner />;
-
   if (errorMessage) return <p>{errorMessage}</p>;
+  if (!places.length) return <p>No result found</p>;
 
-  if (!places?.length) return <p>No result found</p>;
+  const renderCell = (column, place, index) => {
+    if (column.key === "index") {
+      return index + 1;
+    } else if (column.key === "country") {
+      return (
+        <div className={styles.countryData}>
+          <img
+            src={`https://flagsapi.com/${place.countryCode}/flat/16.png`}
+            alt={`${place.country} flag`}
+          />
+          <div>{place[column.key]}</div>
+        </div>
+      );
+    }
+    return place[column.key];
+  };
 
   return (
     <table className={styles.table}>
       <thead>
         <tr className={styles.headerRow}>
-          {columns.map((column) => (
-            <th key={column.key}>{column.label}</th>
+          {columns.map(({ key, label }) => (
+            <th key={key}>{label}</th>
           ))}
         </tr>
       </thead>
@@ -28,20 +43,7 @@ const PlacesTable = ({ places, errorMessage, isLoading }) => {
         {places.map((place, index) => (
           <tr key={place.id}>
             {columns.map((column) => (
-              <td key={column.key}>
-                {column.key === "index" ? (
-                  index + 1
-                ) : column.key === "country" ? (
-                  <div className={styles.countryData}>
-                    <img
-                      src={`https://flagsapi.com/${place.countryCode}/flat/16.png`}
-                    />
-                    <div>{place[column.key]}</div>
-                  </div>
-                ) : (
-                  place[column.key]
-                )}
-              </td>
+              <td key={column.key}>{renderCell(column, place, index)}</td>
             ))}
           </tr>
         ))}
